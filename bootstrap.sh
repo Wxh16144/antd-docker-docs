845aba6d0079bcc8b73ecb92994c4adc2a2ac2f4
wuxh <wxh1220@gmail.com>
2022-12-02 17:13:35 +0800

    perf: update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+temp_dir=$(mktemp -d)
 branch=${1:-master}
-workdir=${2:-\/app}
-proxy=$3
-repo=${proxy}https://github.com/ant-design/ant-design.git
+workdir=${2:-$temp_dir}
+repo_url=${3:-https://github.com/ant-design/ant-design.git}
+npm_registry=${4:-https://registry.npmjs.org/}
 
-git clone --depth 1 -b $branch $repo $workdir && cd $workdir
-npm config set registry https://registry.npm.taobao.org
+git clone --depth 1 -b $branch $repo_url $workdir
+cd $workdir
+
+npm config set registry $npm_registry
 npm install
 npm run site
 
-rm -rf `ls | grep -v '^_site'`
+ls $temp_dir | grep -v '^_site' | xargs rm -rf
